57d4a8d104fd100a684fe934315f614681ccf06c
Lex <alejandromartinalvarez2000@gmail.com>
2021-11-21 23:10:34 +0100

    ej31

--- Boletin UD4/3Objetos/ejercicio31.js
+++ Boletin UD4/3Objetos/ejercicio31.js
@@ -14,6 +14,65 @@ sesionCalificacion.sort(function(a, b){return b - a});
 console.log(sesionCalificacion);
 
 function anadirPiloto(sesionCalificacion,piloto,tiempo) {
-    sesionCalificacion.push(piloto,tiempo);
+    sesionCalificacion.push([piloto,tiempo]);
 }
 
+function ordenarTiempo(arrayCalificaciones) {
+    return arrayCalificaciones.sort(function (a,b) {
+        if (a[1] > b[1]) {
+          return 1;
+        } else if (a[1] < b[1]) {
+          return -1;
+        }
+        return 0;
+      });
+}
+
+function ordenarNombre(arrayCalificaciones) {
+    return arrayCalificaciones.sort(function (a,b) {
+        if (a[0].nombre > b[0].nombre ) {
+          return 1;
+        } else if (a[0].nombre < b[0].nombre) {
+          return -1;
+        } 
+        return 0;
+      });
+}
+
+function nuevaCalificacion(arrayCalificaciones, piloto, tiempo) {
+  var index = arrayCalificaciones.findIndex((sesion) => sesion[0].nombre == piloto.nombre);
+
+  if (index != -1) {
+    if (arrayCalificaciones[index][1] > tiempo) {
+      arrayCalificaciones[index][1] = tiempo;
+    }
+  } else {
+    arrayCalificaciones.push([piloto, tiempo]);
+  }
+  return arrayCalificaciones;
+}
+
+function eliminarCalificacion(arrayCalificaciones, nombre) {
+  var oldLength = arrayCalificaciones.length;
+  arrayCalificaciones = arrayCalificaciones.filter((sesion) => {
+    return sesion[0].nombre !== nombre;
+  });
+  if (oldLength != arrayCalificaciones.length) {
+    console.log("Se ha eliminado un elemento");
+  }
+  return arrayCalificaciones;
+}
+
+// Ordenar por tiempo.
+//console.log(ordenarTiempo(arrayCalificaciones));
+
+var piloto3 = new Piloto("Pepe", "Pajaritos S.A.");
+
+// Crear nueva calificacion.
+//arrayCalificaciones = nuevaCalificacion(arrayCalificaciones, piloto3, 10);
+
+// Eliminar una calificacion.
+arrayCalificaciones = eliminarCalificacion(arrayCalificaciones, "Javier");
+
+// Ordenar por nombre.
+console.log(ordenarNombre(arrayCalificaciones));
