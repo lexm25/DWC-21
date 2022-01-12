bf58b3a3954ae9bf0143b1ed6a2c50670acff1da
lexm25 <alejandro.martin.alvarez.al@iespoligonosur.org>
2022-01-12 14:19:49 +0100

    clase

--- Boletin UD6/ejercicio3.js
+++ Boletin UD6/ejercicio3.js
@@ -1,12 +1,21 @@
 window.addEventListener("load",iniciar);
 
 function iniciar() {
-    var prueba = document.createElement('li');
-    var txt = document.createTextNode('Opel');
-    prueba.appendChild(txt);
-    var para = document.createElement('p');
-    para.appendChild(prueba);
-    document.body.appendChild(para);
-    document.getElementsByTagName('ul').appendChild(prueba);
+    document.getElementsByTagName('li')[0].parentNode.setAttribute("type","square");
+
+    var principio = document.createElement('li');
+    var txt1 = document.createTextNode('Audi');
+    principio.appendChild(txt1);
+    document.getElementsByTagName('li')[0].parentNode.insertBefore(principio,document.getElementsByTagName('li')[0].parentElement.firstChild);
+
+    var final = document.createElement('li');
+    var txt2 = document.createTextNode('Opel');
+    final.appendChild(txt2);
+    document.getElementsByTagName('li')[0].parentNode.appendChild(final);
+
+    var medio = document.createElement('li');
+    var txt3 = document.createTextNode('Renault');
+    medio.appendChild(txt3);
+    document.getElementsByTagName('li')[0].parentNode.insertBefore(medio,document.getElementsByTagName('li')[2]);
 
 }
